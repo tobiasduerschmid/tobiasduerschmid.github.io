70e09c0d2e20c1946bb2c8e207168c5f173db8d0
Tobias Dürschmid <duerschmid@ucla.edu>
2026-03-15 09:39:45 -0700

    Fix highlighting

--- _plugins/markdown_highlighter.rb
+++ _plugins/markdown_highlighter.rb
@@ -1,8 +1,16 @@
-Jekyll::Hooks.register [:posts, :documents], :pre_render do |post|
-  # Only process markdown files to avoid touching assets or layouts
-  if post.path.end_with?(".md", ".markdown")
-    # Replace ==text== with <mark>text</mark>
-    # Using a robust regex that handles punctuation and curly quotes
-    post.content.gsub!(/(?<![=])==([^=\s][^=]*?[^=\s])==(?![=])/, '<mark>\1</mark>')
+Jekyll::Hooks.register [:posts, :pages, :documents], :post_render do |post|
+  # We use a regex that matches the markers and their content
+  # but we perform the replacement only if the markers are in "plain text" areas
+  # of the HTML (not inside tags like <script>, <code>, <pre>, or as attributes).
+  
+  # This pattern matches any tag (<...>), or the sequence ==...==
+  # This allows us to skip over tags entirely.
+  post.output.gsub!(/(<[^>]+?>)|==([^=\s][^=]*?[^=\s])==/) do |match|
+    if $1 # It was a tag
+      match
+    else # It was the highlight syntax
+      content = $2
+      "<mark>#{content}</mark>"
+    end
   end
 end
