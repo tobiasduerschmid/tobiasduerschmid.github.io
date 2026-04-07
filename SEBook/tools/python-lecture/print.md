322c2da30ca10f7d98a79c8d171b144b9fc36f3d
Tobias Dürschmid <duerschmid@ucla.edu>
2026-04-07 12:43:43 -0700

    Fix python lecture print view

--- SEBook/tools/python-lecture/print.md
+++ SEBook/tools/python-lecture/print.md
@@ -2,5 +2,5 @@
 layout: print-tutorial
 title: "Python Lecture"
 tutorial: python-lecture
-permalink: /SEBook/tools/python-lecture
+permalink: /SEBook/tools/python-lecture/print
 ---
--- qr.py
+++ qr.py
@@ -1,5 +1,6 @@
 import qrcode
 
 url = "https://tobiasduerschmid.github.io/SEBook/tools/regex.html#quick-reference"
+url ="https://tobiasduerschmid.github.io/SEBook/tools/python-lecture-tutorial"
 img = qrcode.make(url)
 img.save("regex_tool_qr.png")
