9e7fbd28a2cc672176286ff02def7f2df7128f7f
Tobias Dürschmid <duerschmid@ucla.edu>
2026-05-04 11:07:16 -0700

    Fix nested markdown highlights

--- _plugins/markdown_highlighter.rb
+++ _plugins/markdown_highlighter.rb
@@ -1,25 +1,31 @@
 require 'kramdown'
 
-Jekyll::Hooks.register [:posts, :pages, :documents], :post_render do |post|
-  # Only process if it's an HTML file
-  next unless post.output && post.output.include?("<html") || post.url.end_with?("/", ".html")
+module MarkdownHighlighter
+  SKIP_OR_HIGHLIGHT = %r{<(script|code|pre|style)\b.*?>.*?</\1>|(<[^>]+?>)|==([^=\s][^=]*?[^=\s])==}mi.freeze
 
-  # Tag-skipping regex: skips scripts, code blocks, and individual tags
-  # Matches: 1. Script/Code/Pre blocks, 2. Any HTML tag, 3. The ==highlight== syntax
-  post.output.gsub!(%r{<(script|code|pre|style).*?>.*?</\1>|(<[^>]+?>)|==([^=\s][^=]*?[^=\s])==}mi) do |match|
-    if $1 || $2
-      match # Skip tags and forbidden blocks
-    else
-      # It's our highlight syntax
-      content = $3
-      
-      # Convert basic markdown inside (Italics and Bold)
-      # We do this manually to be fast and avoid double-wrapping in <p> tags
-      # Handling bold first, then italics
-      content.gsub!(/\*\*(.+?)\*\*/, '<strong>\1</strong>')
-      content.gsub!(/\*(.+?)\*/, '<em>\1</em>')
-      
-      "<mark>#{content}</mark>"
+  def self.render_inline_markdown(content)
+    html = Kramdown::Document.new(content, input: 'GFM').to_html.strip
+    html.sub(/\A<p>/, '').sub(%r{</p>\z}, '')
+  end
+
+  def self.process(output)
+    output.gsub(SKIP_OR_HIGHLIGHT) do |match|
+      skipped_block = Regexp.last_match(1)
+      html_tag = Regexp.last_match(2)
+      content = Regexp.last_match(3)
+
+      if skipped_block || html_tag
+        match
+      else
+        "<mark>#{render_inline_markdown(content)}</mark>"
+      end
     end
   end
+end
+
+Jekyll::Hooks.register [:posts, :pages, :documents], :post_render do |post|
+  # Only process if it's an HTML file
+  next unless post.output && (post.output.include?("<html") || post.url.end_with?("/", ".html"))
+
+  post.output = MarkdownHighlighter.process(post.output)
 end
